b92bd1a8dba13da65baf4b53672c947945142ba5
bevane <bevane@live.com>
2024-07-11 11:00:09 +0700

    feat: add check full board func

--- script.js
+++ script.js
@@ -67,7 +67,16 @@ function GameBoard() {
     return false
   }
 
-  return { board, printBoard, addToken, clearBoard, checkWinCondition }
+  function checkFullBoard() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === " ") return false;
+      }
+    }
+    return true
+  }
+
+  return { board, printBoard, addToken, clearBoard, checkWinCondition, checkFullBoard }
 }
 
 
